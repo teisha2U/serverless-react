d4175ce2204a9349e8fb37ca1a01c83c2618cd3b
teisha2U <125391195+teisha2U@users.noreply.github.com>
2023-09-27 11:24:44 -0500

    Update dynamodb.service.ts

--- backend/src/services/dynamodb.service.ts
+++ backend/src/services/dynamodb.service.ts
@@ -74,7 +74,7 @@ export class DynamoService {
   }
 
   /*
-    Implement Query:
+    TODO Implement Query:
     import { QueryCommand } from "@aws-sdk/lib-dynamodb";
 import { ddbDocClient } from "../libs/ddbDocClient.js";
 
@@ -115,82 +115,3 @@ queryTable();
 
 */
 }
-
-/*
-import { DynamoDB } from 'aws-sdk';
-import { DeleteItemOutput, DocumentClient, Key, PutItemOutput } from 'aws-sdk/clients/dynamodb';
-import { Inject } from 'typescript-ioc';
-import { EnvConfigService } from './envConfigService';
-
-/**
- * Utility for accessing S3 objects
- * Changes style from callbacks to promises for clarity
- * Add more functions as needed, try to follow this style
- /
-export class DynamoService {
-    private dynamoDb: DynamoDB.DocumentClient;
-
-    constructor(@Inject config: EnvConfigService) {
-        this.dynamoDb = new DocumentClient({ region: config.getRegion() });
-    }
-
-    public async batchGet(table: string, keys: Key[]): Promise<any[]> {
-        const params = {
-            RequestItems: {
-                [table]: {
-                    Keys: keys,
-                },
-            },
-        };
-
-        const result = await this.dynamoDb.batchGet(params).promise();
-        return result.Responses[table];
-    }
-
-    public async get(table: string, key: any): Promise<any> {
-        const params = {
-            TableName: table,
-            Key: key,
-        };
-
-        const result = await this.dynamoDb.get(params).promise();
-        return result.Item;
-    }
-
-    public async query(table: string, keyConditionExpression: any, expressionAttributes: any): Promise<any> {
-        const params = {
-            TableName: table,
-            KeyConditionExpression: keyConditionExpression, //Example: 'contactId = :contactId',
-            ExpressionAttributeValues: expressionAttributes, // Example: {":contactId": contactId}
-        };
-
-        //Do not use scan - use query
-        const result = await this.dynamoDb.query(params).promise();
-        console.log('Found # of results', result.Count);
-        return result.Items;
-    }
-
-    public put(table: string, data: any): Promise<PutItemOutput> {
-        const params = {
-            Item: data,
-            TableName: table,
-        };
-
-        return this.dynamoDb.put(params).promise();
-    }
-
-    public delete(table: string, key: string, ts: number): Promise<DeleteItemOutput> {
-        const params = {
-            Key: {
-                contactId: key,
-                timeStamp: ts,
-            },
-            TableName: table,
-        };
-
-        return this.dynamoDb.delete(params).promise();
-    }
-
-}
-
-*/
